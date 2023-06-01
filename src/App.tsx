3fcafac3b245f3745bc6ac9bf7cade5db8dac6c0
bluecherrycode <shararehshadlo@gmail.com>
2023-06-01 18:23:09 +0330

    removed switch language in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import LanguageSwitcher from './components/LanguageSwitcher';
 
 function App() {
   const { t } = useTranslation();
   return (
     <>
       <h1>{t('greeting')}</h1>
-      <LanguageSwitcher />
     </>
   );
 }
